3e77ce67f80caaa08ca6ded0293c36d5c3a382bf
Diana <mdiannna@yahoo.com>
2021-02-05 11:33:56 +0200

    Added hints for the questions and added colors_list.txt

--- Lab1/generate_questions.py
+++ Lab1/generate_questions.py
@@ -8,6 +8,13 @@ class QuestionsGenerator():
         pass
         # TODO: generate questions based on rules
         # Examples for generated questions:
+        # Skin color? - daca detecteaza color from colors_list.txt
         # Provide suitable category for [color]
         # Or Yes/no
         # Or range questions (nr)
+
+        #the rest that remain, ask :
+        # The tourist wears a mask? [yes or no]   '(?x) wears a mask', - just replace (?x) with 'the tourist' and add question mark + [yes or no]
+
+
+        
