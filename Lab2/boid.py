a4353bd5a38f3890a26037d545f88fb021dff0fa
Diana <mdiannna@yahoo.com>
2021-02-12 19:52:44 +0200

    Started creating class boid with some methods

---
+++ Lab2/boid.py
@@ -0,0 +1,39 @@
+class Boid():
+    def __init__(self, initial_x, initial_y):
+        """ initialize object """
+        self.__x = initial_x
+        self.__y = initial_y
+        # TODO
+    
+    def separation():
+        """ steer to avoid crowding neighbours (short range repulsion) """
+        pass
+        # TODO
+    
+    def alignment():
+        """  steer towards the average heading of neighbours """
+        pass
+        # TODO
+    
+    def cohesion():
+        """ steer to move towards average position of neighbours (long range attraction) """
+        pass
+        # TODO
+    
+    @property
+    def x(self):
+        return self.__x
+    
+    @property
+    def y(self):
+        return self.__y
+        
+    @x.setter
+    def x(self, new_x):
+        self.__x = new_x
+    
+    @y.setter
+    def y(self, new_y):
+        self.__y = new_y
+
+
